55cd73403277cf61a42443acbc7b08d443dd7383
916-Sandor-Alexandra <alexandra.sandor19@gmail.com>
2023-04-08 16:23:54 +0300

    Added server and authentication

---
+++ server/models/User.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = Schema(
+    {
+        username: {
+            type: String,
+            required: true
+        },
+        surname: {
+            type: String,
+            required: true
+        },
+        forename: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            lowercase:true,
+            trim: true,
+            unique: true,
+            validate: [
+                (value) => /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(value),
+            ]
+        },
+        role: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        refresh_token: String,
+    },
+    {
+        virtuals: {
+            fullName: {
+                get() {
+                    return this.forename + ' ' + this.surname;
+                }
+            },
+            id: {
+                get() {
+                    return this._id;
+                }
+            }
+        }
+    }
+)
+
+module.exports = mongoose.model('User', UserSchema);
